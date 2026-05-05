c24d211f72a9bad61c9b4cd7c7cf18066b113c07
baument <brawlnick70@gmail.com>
2026-05-05 16:16:50 +0300

    Update ClassPoint.h

--- ClassPoint.h
+++ ClassPoint.h
@@ -5,119 +5,41 @@
 using namespace std;
 
 /**
-* @brief Класс "Точка"
-*/
+ * @brief Класс "Точка"
+ */
 class Point
 {
 private:
-	/**
-	* @param x - координата точки по горизонтальной оси
-	*/
-	int x;
+    int x;
+    int y;
 
-	/**
-	* @param y - координата точки по вертикальной оси
-	*/
-	int y;
+    static int max_size_x;
+    static int max_size_y;
 
-	/**
-	* @param max_size_x - максимальное значение которое может принимать параметр x
-	*/
-	static int max_size_x;
-
-	/**
-	* @param max_size_y - максимальное значение которое может принимать параметр y
-	*/
-	static int max_size_y;
-
-	/**
-	* @brief Функция проверки для точки
-	*/
-	void checkPoint(void);
-
-	/**
-	* @brief Функция координаты x
-	* @param x - проверяемая координата
-	* @return Если проверка пройдена - 1, иначе 0
-	*/
-	bool checkX(const int x);
-
-	/**
-	* @brief Функция координаты y
-	* @param y - проверяемая координата
-	* @return Если проверка пройдена - 1, иначе 0
-	*/
-	bool checkY(const int y);
+    void checkPoint(void);
+    bool checkX(const int x);
+    bool checkY(const int y);
 
 public:
-	/**
-	* @brief Базовый конструктор класса, меняет значения x и y на указанные пользователем
-	* @param x - передаваемое значение по оси x (по умолчанию 0)
-	* @param y - передаваемое значение по оси y (по умолчанию 0)
-	*/
-	Point(const int x = 0, const int y = 0);
+    Point(const int x = 0, const int y = 0);
 
-	/**
-	* @brief Получает значение по оси x у конкретной точки
-	* @return Значение по оси x
-	*/
-	int getX(void) const;
+    Point(const Point& p);
+    Point(Point&& p);
 
-	/**
-	* @brief Получает значение по оси y у конкретной точки
-	* @return Значение по оси y
-	*/
-	int getY(void) const;
+    int getX(void) const;
+    int getY(void) const;
 
-	/**
-	* @brief Задаёт максимальные значения для x и y
-	* @param x - максимальное значение по x
-	* @param y - максимальное значение по y
-	*/
-	static void getScreenDimensions(const int x, const int y);
+    static void getScreenDimensions(const int x, const int y);
 
-	/**
-	* @brief Получает максимальное значение по x
-	* @return Максимальное значение по x
-	*/
-	static int get_max_size_x(void);
+    static int get_max_size_x(void);
+    static int get_max_size_y(void);
 
-	/**
-	* @brief Получает максимальное значение по y
-	* @return Максимальное значение по y
-	*/
-	static int get_max_size_y(void);
+    void operator=(const Point& other);
+    void operator=(Point&& other);
 
-	/**
-	* @brief Оператор приравнивания, присваивает значение координат другой точке
-	* @param other - точка, на чьи значения будет происходить замена
-	*/
-	void operator = (const Point& other);
+    bool operator==(const Point& other) const;
+    bool operator!=(const Point& other) const;
 
-	/**
-	* @brief Оператор равенства, точки считаются равными, если их координаты попарно равны
-	* @param other - точка, с которой происходит сравнение
-	*/
-	bool operator == (const Point& other) const;
-
-	/**
-	* @brief Оператор равенства, точки считаются не равными, если их координаты попарно не равны
-	* @param other - точка, с которой происходит сравнение
-	*/
-	bool operator != (const Point& other) const;
-
-	/**
-	* @brief Оператор вывода для класса точка
-	* @param output - ссылка на поток вывода
-	* @param point - ссылка на выводимую точку
-	* @return Координаты точки в формате (x,y)
-	*/
-	friend ostream& operator << (ostream& output, const Point& point);
-
-	/**
-	* @brief Оператор ввода для класса точка
-	* @param input - ссылка на поток ввода
-	* @param point - ссылка на вводимую точку
-	*/
-	friend istream& operator >> (istream& input, Point& point);
+    friend ostream& operator<<(ostream& output, const Point& point);
+    friend istream& operator>>(istream& input, Point& point);
 };
