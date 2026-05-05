81995e48e24a53b34fa3b643397d8fe679b4cff2
baument <brawlnick70@gmail.com>
2026-05-05 16:19:34 +0300

    Update ClassEllipse.h

--- ClassEllipse.h
+++ ClassEllipse.h
@@ -4,55 +4,28 @@
 #include "ClassPoint.h"
 
 /**
-* @brief Класс "Овал"
-*/
+ * @brief Класс "Овал"
+ */
 class Ellipse : public Shape
 {
 private:
-	/**
-	* @param R - точка-центр овала
-	*/
-	Point R;
+    Point R;
+    int a;
+    int b;
 
-	/**
-	* @param a - первая полуось
-	*/
-	int a;
-
-	/**
-	* @param b - вторая полуось
-	*/
-	int b;
-
-	/**
-	* @brief Проверка на правильное задание Овала
-	*/
-	void CheckEllipse(void);
+    void CheckEllipse(void);
 
 public:
-	/**
-	* @brief Базовый конструктор класса, задаёт овал через центр и две полуоси
-	* @param R - точка-центр овала
-	* @param a - первая полуось
-	* @param b - вторая полуось
-	*/
-	Ellipse(const Point R, const int a, const int b);
+    Ellipse(const Point R, const int a, const int b);
 
-	/**
-	* @brief Виртуальная функция сериализации в строку для класса овал
-	* @param os - поток вывода
-	*/
-	void ToString(ostream& os) override;
+    Ellipse(const Ellipse& e);
+    Ellipse(Ellipse&& e);
 
-	/**
-	* @brief Виртуальная функция расчёта периметра для класса овал
-	* @return Периметр овала
-	*/
-	double getPerimeter() override;
+    Ellipse& operator=(const Ellipse& e);
+    Ellipse& operator=(Ellipse&& e);
 
-	/**
-	* @brief Виртуальная функция расчёта площади для класса овал
-	* @return Площадь овала
-	*/
-	double getArea() override;
+    void ToString(ostream& os) override;
+
+    double getPerimeter() override;
+    double getArea() override;
 };
